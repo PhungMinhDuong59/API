5cb710747d5872808b2fdcd27345523569ee2e33
phung minh duong <phungminhduong59@gmail.com>
2025-04-20 20:38:13 +0700

    Add project files.

---
+++ API/Repository/IStudentRepo.cs
@@ -0,0 +1,13 @@
+﻿using API.Models;
+
+namespace API.Repository
+{
+    public interface IStudentRepo
+    {
+        Task<List<Student>> GetAll();
+        Task<Student> GetById(int id);
+        Task Create(Student student);
+        Task Update(Student student);
+        Task Delete(int id);
+    }
+}
---
+++ API/Repository/StudentRepo.cs
@@ -0,0 +1,47 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repository
+{
+    public class StudentRepo : IStudentRepo
+    {
+        private readonly AppDbContext _context;
+        public StudentRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Student>> GetAll()
+        {
+            return await _context.Students.ToListAsync();
+        }
+
+        public async Task<Student> GetById(int id)
+        {
+            return await _context.Students.FindAsync(id);
+        }
+
+        public async Task Create(Student student)
+        {
+            await _context.Students.AddAsync(student);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(Student student)
+        {
+            _context.Students.Update(student);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
